cc07cd2857da4ff83e4908597b3aa7005c6f9178
miyazaki <miyazakita@cy-com.co.jp>
2025-06-04 13:54:10 +0900

    test commit

---
+++ sample-fortune/src/main/java/com/example/sample/sample_fortune/controller/SampleController.java
@@ -0,0 +1,37 @@
+package com.example.sample.sample_fortune.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.sample.sample_fortune.service.SampleService;
+
+
+
+@Controller
+public class SampleController {
+    private final SampleService sampleService;
+
+    public SampleController(SampleService sampleService){
+        this.sampleService = sampleService;
+    }
+    
+    @PostMapping("/result")
+    public String sample(@RequestParam String name,@RequestParam int age, Model model) {
+        String result = sampleService.getFortune();
+        model.addAttribute("result", result);
+
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "sample";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "form";
+    }
+    
+    
+}
